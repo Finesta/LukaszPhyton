931a1c217e5ce4fb428800c44a5569afc6374764
przywozny <lprzywony@gmail.com>
2020-02-29 12:00:12 +0100

    ide do domu

---
+++ start.py
@@ -0,0 +1,34 @@
+print("Hello World!")
+
+# def main():
+    # for elem in kolekcja:
+        # costam()
+
+# lańcuchy znaków
+
+liczba = "Ala"
+liczba = 0
+liczba = 0.0
+
+# PEP8
+
+imie = "Ala"
+print(imie)
+print(type(imie))
+print(type(5))
+print(type(4.4))
+print(type(True))
+print(type(None)) #null
+
+imie = str("Ala")
+liczba = str(100) # "100"
+
+binarnie = "101"
+print(int(binarnie, 2))
+
+imie = imie.lower()
+# imie[0] = 'a' blad
+print(imie)
+
+"Ala".lower().title()
+
